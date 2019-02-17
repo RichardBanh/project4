0b2f5faf06fe30d05e6a4edd18d9de3ed634823e
rbanh <rbanh@me.com>
2019-02-16 18:11:59 -0800

    put assets in stoopid public folder

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -2,12 +2,12 @@ import React, { Component } from 'react';
 class Nav extends Component {
  render() { 
   return (  
-   <div class="navigation">
-        <div class= "inner">
-            <a href="./index.html"><img src='../src/assets/Logo/Logo-brainflix.svg' alt="logo"/></a>
+   <div className="navigation">
+        <div className= "inner">
+            <a href="./index.html"><img src="./Logo/Logobrainflix.svg" alt="logo"/></a>
             <nav>
                 <ul>
-                    <li class="active"><a href="#">Biography</a></li>
+                    <li className="active"><a href="#">Biography</a></li>
                     <li><a href="./shows.html">Shows</a></li>
                 </ul>
             </nav>
