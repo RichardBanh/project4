6902a218fbb7ba8858132f3e337e30e2e1cded17
rbanh <rbanh@me.com>
2019-02-20 22:52:28 -0800

    mark here for sprint 2

--- src/App.js
+++ src/App.js
@@ -1,27 +1,16 @@
 import React, { Component } from 'react';
 import Nav from './components/navbar';
-import Video from './components/video';
-import Videotitle from './components/videotitle.jsx'
-import Form from './components/form';
-import CommentL from './components/commentlist.jsx'
-import Videolist from './components/videolist.jsx'
+import Videosection from './components/videosection'
+import { BrowserRouter, Route, Switch, Link } from 'react-router-dom'
 
 class App extends Component {
   render() {
-    console.log(this.props.mainVideo[0]);
     return (
       <div>
         <Nav />
-        <Video />
-        
-        <div className="flexing2">
-          <div>
-            <Videotitle mainVideo = {this.props.mainVideo[0]} />
-            <Form comlength = {this.props.mainVideo[0].comments}/>
-            <CommentL comments = {this.props.mainVideo[0].comments}/>
-          </div>
-          <Videolist sugs = {this.props.sideVideo}/>
-        </div>
+        <BrowserRouter>
+          <Videosection mainVideo={this.props.mainVideo} sideVideo={this.props.sideVideo}/>
+        </BrowserRouter>
       </div>
     );
     }  
